9120df6ab9494c3c3e27d67f89fac79f08faeaca
Tejodeep Mitra Roy <tejodeepmitraroy2002@gmail.com>
2024-02-29 10:09:07 +0530

    fix: eslint error

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,18 +18,22 @@ const config = {
 			},
 		},
 		extend: {
+			fontFamily: {
+				poppins: 'Poppins, sans-serif',
+			},
 			colors: {
 				border: 'hsl(var(--border))',
 				input: 'hsl(var(--input))',
 				ring: 'hsl(var(--ring))',
-				background: 'hsl(var(--background))',
-				foreground: 'hsl(var(--foreground))',
+				background: 'var(--background)',
+				background2: 'var(--background2)',
+				foreground: 'var(--foreground)',
 				primary: {
-					DEFAULT: 'hsl(var(--primary))',
+					DEFAULT: 'var(--primary)',
 					foreground: 'hsl(var(--primary-foreground))',
 				},
 				secondary: {
-					DEFAULT: 'hsl(var(--secondary))',
+					DEFAULT: 'var(--secondary)',
 					foreground: 'hsl(var(--secondary-foreground))',
 				},
 				destructive: {
