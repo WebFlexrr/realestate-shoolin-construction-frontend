997a1976a5f8851c78227d03b08d7bbf0f62bb54
Tejodeep Mitra Roy <tejodeepmitraroy2002@gmail.com>
2024-09-10 03:17:08 +0530

    feat: add not-found page & privacy page

--- src/app/api/enquiry/route.ts
+++ src/app/api/enquiry/route.ts
@@ -1,1 +1,5 @@
-export async function POST(request: Request) {}
+import { NextResponse } from 'next/server';
+
+export async function GET(request: Request) {
+	return NextResponse.json('OK server');
+}
